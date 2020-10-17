d45d73e4076821b533445ae3657436d0e9c439ba
btorres136 <btorres136@emqail.suagm.edu>
2020-10-17 00:26:08 -0400

    reorganization and dockerized

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./resources/SCSS/main.scss";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <p>hello World!👋👋</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
