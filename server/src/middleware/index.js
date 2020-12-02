c9321f91c67358c647f9559698d7685822b476c7
btorres136 <dtdev98@gmail.com>
2020-12-01 22:51:44 -0400

    some changes

--- server/src/middleware/index.js
+++ server/src/middleware/index.js
@@ -3,6 +3,11 @@ module.exports = function (app) {
   // Add your custom middleware here. Remember that
   // in Express, the order matters.
   app.get('/Spine3D', (req, res) => {
+    res.setHeader(
+      'Content-Security-Policy',
+      "default-src * 'unsafe-inline' ;img-src 'self' data:"
+    )
+
     res.render('spine', {Brian: 'hello'});
   });
 };
